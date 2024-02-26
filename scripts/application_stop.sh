d307369f47cc25229c75db64a5487e812f55ca5c
ewerton0 <54162965+ewerton0@users.noreply.github.com>
2024-02-26 15:13:17 -0300

    Add files via upload

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,7 +2,7 @@
 
 echo "This Script is used to stop already running docker container, remove them and remove the image as well"
 
-sudo docker stop $(sudo docker ps -q)
+sudo docker stop $(docker ps -a -q)
 
 sudo docker rm $(sudo docker ps -a -q)
 
